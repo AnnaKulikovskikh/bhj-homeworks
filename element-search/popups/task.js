728e44df47b8ccf40fdb5ba3b7984a9ef796a332
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-06-23 23:26:21 +0300

    Update task.js

---
+++ element-search/popups/task.js
@@ -0,0 +1,15 @@
+const modal = document.getElementById('modal_main');
+const success = document.getElementById('modal_success');
+const save = modal.className;
+
+modal.className = 'modal_active';
+const close = document.getElementsByClassName('modal__close');
+close[1].onclick = function(){
+  modal.className =save;
+  success.className = 'modal_active';
+  console.log(modal.className);
+}
+close[2].onclick = function(){
+  success.className = save;
+}
+console.log(modal.className);
