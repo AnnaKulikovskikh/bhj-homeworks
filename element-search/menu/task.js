c8d4cab607b2c0c1199de5c3daf764d79665a95b
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-07-06 21:38:09 +0300

    Update task.js

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,11 +1,11 @@
 const link = Array.from(document.querySelectorAll('.menu__link'));
 
 function handler() {
-  if (document.querySelector('.menu_active') != null) {
+  if (document.querySelector('.menu_active') != null && this.nextElementSibling == null) {
     document.querySelector('.menu_active').classList.remove('menu_active');
   }
   if (this.nextElementSibling != null) {
-    this.nextElementSibling.classList.add('menu_active');
+    this.nextElementSibling.classList.toggle('menu_active');
     event.preventDefault();
   } 
 }
