c94490d779532e8dbee736acc4ee63392d142a2e
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-07-06 22:59:02 +0300

    Update task.js

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -12,33 +12,28 @@ dots.forEach((b) => {
   b.addEventListener('click', clicker)
 });
 
+function remove(number){
+  dots[number].classList.remove('slider__dot_active');
+  item[number].classList.remove('slider__item_active');
+}
+
 function clicker(){
-  dots.forEach((b) => b.classList.remove('slider__dot_active'));
+  remove(image);
   this.classList.add('slider__dot_active');
-  for (i  = 0;i < dots.length;i++){
-    if (this === dots[i]) {
-      item.forEach((b) => b.classList.remove('slider__item_active'));
-      item[i].classList.add('slider__item_active');
-      image = i;
-    }
-  }
+  image = dots.findIndex(it => it == this);
+  item[image].classList.add('slider__item_active');
 }
 
 function handler(){
-  //console.log(this.className);
-  item[image].className = 'slider__item';
+  remove(image);
   if (this.className == 'slider__arrow slider__arrow_next') {
-    dots[image].classList.remove('slider__dot_active');
     image += 1; 
     if (image == item.length) {image = 0};
-    dots[image].classList.add('slider__dot_active');
   } 
   if (this.className == 'slider__arrow slider__arrow_prev') {
-    dots[image].classList.remove('slider__dot_active');
     image -=1;
     if (image == -1) {image = item.length-1};
-    dots[image].classList.add('slider__dot_active');
   }
-  //console.log(image);
+  dots[image].classList.add('slider__dot_active');
   item[image].classList.add('slider__item_active');
 }
