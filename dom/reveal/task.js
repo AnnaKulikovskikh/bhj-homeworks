111f818dae9ef4945f6fb7a22d077f1ac02f0ade
AnnaKulikovskikh <59337789+AnnaKulikovskikh@users.noreply.github.com>
2020-07-20 18:52:38 +0200

    Update task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,10 +1,14 @@
+//event.preventDefault()
 const reveal = Array.from(document.querySelectorAll('.reveal'));
-let coord = reveal.map(el=>el.getBoundingClientRect());
+//let coord = reveal.map(el=>el.getBoundingClientRect());
 
 window.addEventListener('scroll', function() {
-  let scrolling = window.pageYOffset;
+  //let scrolling = window.pageYOffset;
+  let scroll = window.innerHeight;
+  let coord = reveal.map(el=>el.getBoundingClientRect());
+
   for (i=1;i<reveal.length;i++){
-    if (coord[i].top - 50 < scrolling) {
+    if (coord[i].top - 50 < scroll) {
     reveal[i].classList.add('reveal_active');
     }
   }
